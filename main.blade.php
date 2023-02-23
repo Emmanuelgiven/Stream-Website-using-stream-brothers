b74c17da2d0c2e8619cd1b900d00aa3bc857c7e1
Emmanuelgiven <60091432+Emmanuelgiven@users.noreply.github.com>
2023-02-23 12:17:19 +0300

    main.blade.php

---
+++ main.blade.php
@@ -0,0 +1,3 @@
+  <div style="position: relative; overflow: hidden; width: 100%;padding-top: 56.25%;">
+      <iframe width="560" height="300" src="https://media.streambrothers.com:2000/VideoPlayer/xxxx" style="position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;" scrolling="no" frameborder="0" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+  </div>
